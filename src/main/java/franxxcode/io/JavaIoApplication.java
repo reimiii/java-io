0b76c1c1592a9c16dcd229838310e8ea5e250fff
Hilmi AM <imiia75775@proton.me>
2023-12-31 13:39:48 +0700

    Initial commit

---
+++ src/main/java/franxxcode/io/JavaIoApplication.java
@@ -0,0 +1,13 @@
+package franxxcode.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIoApplication.class, args);
+	}
+
+}
