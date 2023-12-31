89406850415e4080467545124491fdb851aedb37
Hilmi AM <imiia75775@proton.me>
2023-12-31 13:48:41 +0700

    File

---
+++ src/test/java/franxxcode/io/FileTest.java
@@ -0,0 +1,30 @@
+package franxxcode.io;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class FileTest {
+
+    @Test
+    void createFile() {
+
+        File file = new File("file.txt");
+
+        Assertions.assertEquals("file.txt", file.getName());
+        Assertions.assertFalse(file.exists());
+
+    }
+
+
+    @Test
+    void createFileExist() {
+
+        File file = new File("src/main/resources/application.properties");
+
+        Assertions.assertEquals("application.properties", file.getName());
+        Assertions.assertTrue(file.exists());
+
+    }
+}
