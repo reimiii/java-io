4c98ad2389c66ebd733f3161b95ebba4bef5e16f
Hilmi AM <imiia75775@proton.me>
2024-01-01 11:09:52 +0700

    Input Stream byte for non text file

---
+++ src/test/java/franxxcode/io/InputStreamTest.java
@@ -0,0 +1,58 @@
+package franxxcode.io;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class InputStreamTest {
+
+    @Test
+    void read() {
+
+        try (InputStream stream = Files.newInputStream(Path.of("pom.xml"))) {
+
+            StringBuilder builder = new StringBuilder();
+            int data;
+            int counter = 0;
+
+            while ((data = stream.read()) != -1) {
+                builder.append((char) data);
+                counter += 1;
+            }
+
+            System.out.println(builder.toString());
+            System.out.println(counter);
+
+        } catch (IOException e) {
+            Assertions.fail(e);
+        }
+    }
+    @Test
+    void readBytes() {
+
+        try (InputStream stream = Files.newInputStream(Path.of("pom.xml"))) {
+
+            StringBuilder builder = new StringBuilder();
+
+            byte[] buffer = new byte[1024];
+
+            int length;
+            int counter = 0;
+
+            while ((length = stream.read(buffer)) != -1) {
+                builder.append(new String(buffer, 0, length));
+                counter += 1;
+            }
+
+            System.out.println(builder.toString());
+            System.out.println(counter);
+
+        } catch (IOException e) {
+            Assertions.fail(e);
+        }
+    }
+}
